3a5c40b307f4477c7c4d348208593a6e40d526aa
greentl0814 <kj0815@gmail.com>
2024-12-20 13:51:03 +0900

    Add Google Analytics and AdSense to _app.js_fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,54 @@
 import "../styles/globals.css";
-import Footer from '../components/Footer'
-import Script from 'next/script'
+import Footer from "../components/Footer";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
- return (
-   <div className="min-h-screen flex flex-col">
-     <Script
-       src="https://www.googletagmanager.com/gtag/js?id=G-T0EEHV122X"
-       strategy="afterInteractive"
-     />
-     <Script id="google-analytics" strategy="afterInteractive">
-       {`
-         window.dataLayer = window.dataLayer || [];
-         function gtag(){dataLayer.push(arguments);}
-         gtag('js', new Date());
-         gtag('config', 'G-T0EEHV122X');
-       `}
-     </Script>
-     <Script
-       async
-       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6071061687711848"
-       strategy="afterInteractive"
-       crossOrigin="anonymous"
-     />
-     <main className="flex-grow">
-       <Component {...pageProps} />
-     </main>
-     <Footer />
-   </div>
- );
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-T0EEHV122X"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-T0EEHV122X');
+        `}
+      </Script>
+
+      {/* Google AdSense */}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6071061687711848"
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+      />
+      <Script id="adsbygoogle-init" strategy="afterInteractive">
+        {`
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        `}
+      </Script>
+
+      <main className="flex-grow">
+        <Component {...pageProps} />
+        {/* 광고 블록 */}
+        <div className="ad-container">
+          <ins
+            className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-6071061687711848"
+            data-ad-slot="1234567890"
+            data-ad-format="auto"
+          ></ins>
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default MyApp;
