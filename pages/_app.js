38c515322bb69c46faa07af6739d3133f78dcb02
greentl0814 <kj0815@gmail.com>
2025-08-07 15:54:52 +0900

    애드센스 spa 이동 클로드 방식

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,28 @@ import Footer from "../components/Footer";
 import Script from "next/script";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
  const router = useRouter();
+
+ useEffect(() => {
+   const handleRouteChange = () => {
+     try {
+       if (typeof window !== 'undefined' && window.adsbygoogle) {
+         (window.adsbygoogle = window.adsbygoogle || []).push({});
+       }
+     } catch (e) {
+       console.error('AdSense refresh error:', e);
+     }
+   };
+
+   router.events.on('routeChangeComplete', handleRouteChange);
+   return () => {
+     router.events.off('routeChangeComplete', handleRouteChange);
+   };
+ }, [router.events]);
+
  return (
    <div className="min-h-screen flex flex-col">
      <Head>
@@ -28,6 +47,14 @@ function MyApp({ Component, pageProps }) {
          gtag('config', 'G-T0EEHV122X');
        `}
      </Script>
+
+     {/* Google AdSense */}
+     <Script
+       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6071061687711848"
+       strategy="afterInteractive"
+       crossOrigin="anonymous"
+     />
+
      <main className="flex-grow">
        <Component {...pageProps} />
      </main>
