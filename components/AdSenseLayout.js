fbdaf426003e65a0afb9df520af1d7f5d008ea07
greentl0814 <kj0815@gmail.com>
2025-12-05 11:05:32 +0900

    메인 w-4xl

--- components/AdSenseLayout.js
+++ components/AdSenseLayout.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function AdSenseLayout({ children }) {
   return (
     <div className="max-w-7xl mx-auto px-4">
-      {children}
+      <div className="max-w-4xl mx-auto">
+        {children}
+      </div>
     </div>
   );
 }
