cdf5f038d4eab352865408a1c69a0dec170da943
greentl0814 <kj0815@gmail.com>
2026-07-06 21:58:16 +0900

    유튜브 댓글 다운 pikk로 리다이렉트

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,12 @@ const nextConfig = {
         destination: 'https://www.mktbox.co.kr/:path*',
         statusCode: 301
       },
+      {
+        // 알림창이나 꼬리표 없이 pikk의 기능 페이지로 직행
+        source: '/youtube-comments',
+        destination: 'https://pikk.co.kr/tools/youtube',
+        permanent: true,
+      },
     ];
   },
 };
