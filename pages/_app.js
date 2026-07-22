4b6b8f50da29534f70474bf25de7fb3c882c12f0
greentl0814 <kj0815@gmail.com>
2026-07-22 12:40:35 +0900

    애드센스 수정

--- pages/_app.js
+++ pages/_app.js
@@ -7,54 +7,54 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
- const router = useRouter();
+  const router = useRouter();
 
- return (
-   <div className="min-h-screen flex flex-col">
-     <Head>
-       <meta name="viewport" content="width=device-width, initial-scale=1" />
-       <link
-         rel="canonical"
-         href={`https://www.mktbox.co.kr${router.asPath}`}
-       />
-     </Head>
-     {/* Google Analytics */}
-     <Script
-       src="https://www.googletagmanager.com/gtag/js?id=G-T0EEHV122X"
-       strategy="afterInteractive"
-     />
-     <Script id="google-analytics" strategy="afterInteractive">
-       {`
-         window.dataLayer = window.dataLayer || [];
-         function gtag(){dataLayer.push(arguments);}
-         gtag('js', new Date());
-         gtag('config', 'G-T0EEHV122X');
-       `}
-     </Script>
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="canonical"
+          href={`https://www.mktbox.co.kr${router.asPath}`}
+        />
+        {/* Google AdSense (에러 수정: Head 안의 순수 script 태그로 이동) */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6071061687711848"
+          crossOrigin="anonymous"
+        />
+      </Head>
 
-     {/* Google AdSense */}
-     <Script
-       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6071061687711848"
-       strategy="afterInteractive"
-       crossOrigin="anonymous"
-     />
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-T0EEHV122X"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-T0EEHV122X');
+        `}
+      </Script>
 
-     {/* Kakao Ad Script */}
-     <Script
-       src="//t1.daumcdn.net/kas/static/ba.min.js"
-       strategy="afterInteractive"
-       async
-     />
+      {/* Kakao Ad Script */}
+      <Script
+        src="//t1.daumcdn.net/kas/static/ba.min.js"
+        strategy="afterInteractive"
+        async
+      />
 
-     <Header />
-     <main className="flex-grow">
-       <AdSenseLayout>
-         <Component {...pageProps} />
-       </AdSenseLayout>
-     </main>
-     <Footer />
-   </div>
- );
+      <Header />
+      <main className="flex-grow">
+        <AdSenseLayout>
+          <Component {...pageProps} />
+        </AdSenseLayout>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default MyApp;
